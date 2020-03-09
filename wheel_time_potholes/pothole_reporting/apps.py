48bfbad18e90a678436ef2984db78cbfc524242d
Frankie Holzapfel <frankjh3@gmail.com>
2020-03-08 22:19:41 -0500

    Initialize django project with app
    Create default django project using startproject, and create an empty
    app inside using startapp.

---
+++ wheel_time_potholes/pothole_reporting/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PotholeReportingConfig(AppConfig):
+    name = 'pothole_reporting'
---
+++ wheel_time_potholes/wheel_time_potholes/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for wheel_time_potholes project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wheel_time_potholes.settings')
+
+application = get_asgi_application()
