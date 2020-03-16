8b173bb62072e8a7bfbbe111779c39c5767499c4
Isaac <isaacjpoulin@gmail.com>
2020-03-15 19:45:17 -0500

    Added sample query to index page

--- wheel_time_potholes/pothole_reporting/views.py
+++ wheel_time_potholes/pothole_reporting/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+import database as db
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html', context=None)
+    query = "SELECT * FROM pothole"
+    cursor = db.connection.cursor()
+    cursor.execute(query)
+    response = dict()
+    response['results'] = cursor.fetchall()
+    response['length'] = cursor.rowcount
+    response['warnings'] = cursor.fetchwarnings()
+    return render(request, 'index.html', context=response)
