f35c6bd07c881f25d738889881bb5685b933b1eb
Frankie Holzapfel <frankjh3@gmail.com>
2020-03-16 22:42:09 -0500

    Routing to index and image submission pages

--- wheel_time_potholes/wheel_time_potholes/settings.py
+++ wheel_time_potholes/wheel_time_potholes/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pothole_reporting.apps.PotholeReportingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
