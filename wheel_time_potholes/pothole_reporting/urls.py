6afe7535ac8e1250131ece338b8e164b8e6ddfac
Bishwo karki <bishwokarki@unomaha.edu>
2020-04-05 21:49:50 -0500

    Added Static Files

--- wheel_time_potholes/pothole_reporting/urls.py
+++ wheel_time_potholes/pothole_reporting/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
-from . import views
+from . import views,forms
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('pothole-picture/', views.pothole_picture, name='pothole-picture'),
     path('pothole-geojson/', views.pothole_geojson, name='pothole-geojson'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name='pothole_reporting/login.html',authentication_form=forms.login_form), name='login'),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- wheel_time_potholes/pothole_reporting/views.py
+++ wheel_time_potholes/pothole_reporting/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from PIL import UnidentifiedImageError
 
 from pothole_reporting.potholes.geotag_image import create_pothole_by_image
 from pothole_reporting.potholes.geo_potholes import get_geojson_potholes
-from .forms import PotholeImageForm
+from .forms import (
+    PotholeImageForm,
+    login_form,
+    signup_form)
 from .exceptions import NoExifDataError
 
 
@@ -13,6 +18,35 @@ def index(request):
     return render(request,
                   'pothole_reporting/index.html',
                   {"api_key": settings.MAP_API_KEY})
+
+
+def login_view(request):
+    form = login_form(request.POST)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('index') #redirect to the homepage for now
+        else:
+            messages.info(request,'username or password incorrect!')
+    
+    return render(request,"pothole_reporting/login.html")
+
+
+def signup_view(request):
+    form = signup_form(request.POST or None)
+    if request.method == "POST":
+        form = signup_form(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account was created sucessfully for {username}!')
+            return redirect('login')
+        else:
+            form = signup_form()
+    return render(request,"pothole_reporting/signup.html", {'form':form})
 
 
 def pothole_picture(request):
